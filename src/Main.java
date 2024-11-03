10ca8e7a74c39d928c42e189dc4e2da33af3c115
JamalJavadov <camalcavadovwork@gmail.com>
2024-11-03 20:47:28 +0400

    task1 polindrom number

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 import java.util.Scanner;
-
+//Polindrom eded
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int number2 = number;
+        int terseded=0;
+        while(number>0){
+            terseded=(number%10)+terseded*10;
+            number=number/10;
 
+        }
+        String s = terseded == number2 ? "this is polindrom" : "this is not polindrom";
+        System.out.println(s);
 
 
 
